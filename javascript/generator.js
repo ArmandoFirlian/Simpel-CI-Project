9a1981d59c3581ba1e4bcd4ec15f53b03ffe0275
Armando <juliantogusse70@gmail.com>
2026-02-24 20:14:16 +0700

    Update generator.js

--- javascript/generator.js
+++ javascript/generator.js
@@ -18,8 +18,11 @@ export function buatSolusi() {
   }
 
   const rBase = [0, 1, 2];
-  const rows = shuffle(rBase.flatMap(g => shuffle(rBase).map(r => g * base + r)));
-  const cols = shuffle(rBase.flatMap(g => shuffle(rBase).map(c => g * base + c)));
+  const rows = shuffle(rBase).flatMap(g =>
+  shuffle(rBase).map(r => g * base + r));
+
+  const cols = shuffle(rBase).flatMap(g =>
+  shuffle(rBase).map(c => g * base + c));
   const nums = shuffle([1,2,3,4,5,6,7,8,9]);
 
   return rows.map(r =>
